592ed2c08cec4ecf5d92fefdadc4655b369fbdd8
themonomers <54368885+themonomers@users.noreply.github.com>
2021-03-28 20:03:26 -0700

    Create RemoveChargingCron.py

---
+++ python/RemoveChargingCron.py
@@ -0,0 +1,26 @@
+from crontab import CronTab
+
+##
+#
+#
+# author: mjhwa@yahoo.com
+##
+def main():
+  cron = CronTab(user='pi')
+  job = cron.find_command('/home/pi/tesla/ChargeM3.py')
+  cron.remove(job)
+  cron.write()
+  job = cron.find_command('/home/pi/tesla/ChargeM3Backup.py')
+  cron.remove(job)
+  cron.write()
+
+  cron = CronTab(user='pi')
+  job = cron.find_command('/home/pi/tesla/ChargeMX.py')
+  cron.remove(job)
+  cron.write()
+  job = cron.find_command('/home/pi/tesla/ChargeMXBackup.py')
+  cron.remove(job)
+  cron.write()
+
+if __name__ == "__main__":
+  main()
