4f2d9ea259e61ee5134c4baf2f60c9411407db5b
themonomers <54368885+themonomers@users.noreply.github.com>
2021-03-31 16:18:29 -0700

    Create PreconditionMXStop.py

---
+++ python/PreconditionMXStop.py
@@ -0,0 +1,22 @@
+import time
+
+from TeslaVehicleAPI import getVehicleData
+from Utilities import isVehicleAtHome
+from Logger import logError
+
+MX_VIN = ''
+WAIT_TIME = 30
+
+def main():
+  try:
+    data = getVehicleData(MX_VIN)
+    if (isVehicleAtHome(data)): # no need to execute if unsure where the car is or if it's in motion
+      preconditionCarStop(MX_VIN)
+  except Exception as e:
+    logError('preconditionMXStop(): ' + e)
+    wakeVehicle(MX_VIN)
+    time.sleep(WAIT_TIME)
+    preconditionMXStop()
+
+if __name__ == "__main__":
+  main()
