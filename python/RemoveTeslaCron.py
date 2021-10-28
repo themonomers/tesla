f328c894b1556127406ba046361b83e185369d08
Michael Hwa <mjhwa@yahoo.com>
2021-10-28 15:14:05 -0700

    Removed charging cron now that it can be set directly via API.

--- python/RemoveTeslaCron.py
+++ python/RemoveTeslaCron.py
@@ -2,29 +2,13 @@ from crontab import CronTab
 
 
 ##
-# Script to clean up crontabs created for Tesla Smart Charging, Smart Climate,
-# and Telemetry.  Should be set to run in the middle of the day as all the 
-# crontabs are evening or early morning.
+# Script to clean up crontabs created for Tesla Smart Climate.  Should be 
+# set to run in the middle of the day as all the crontabs are evening or 
+# early morning.
 #
 # author: mjhwa@yahoo.com
 ##
 def main():
-  cron = CronTab(user='pi')
-  job = cron.find_command('python /home/pi/tesla/python/ChargeM3.py')
-  cron.remove(job)
-  cron.write()
-  job = cron.find_command('python /home/pi/tesla/python/ChargeM3Backup.py')
-  cron.remove(job)
-  cron.write()
-
-  cron = CronTab(user='pi')
-  job = cron.find_command('python /home/pi/tesla/python/ChargeMX.py')
-  cron.remove(job)
-  cron.write()
-  job = cron.find_command('python /home/pi/tesla/python/ChargeMXBackup.py')
-  cron.remove(job)
-  cron.write()
-
   cron = CronTab(user='pi')
   job = cron.find_command('python /home/pi/tesla/python/PreconditionM3Start.py')
   cron.remove(job)
