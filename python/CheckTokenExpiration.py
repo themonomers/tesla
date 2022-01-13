050433f0e433c954c1a822b5b7b52b3fa3d57475
Michael Hwa <mjhwa@yahoo.com>
2022-01-12 19:26:52 -0800

    More changes for access token updates.
    Not only has Tesla changed the format of the access token, the old method of retieving a new access token from the refresh token has also changed.  Also, the access token no longer lasts for 45 days.  The new access token expires in 8 hours.  The refresh token expiration is unknown and seems to stay constant even after getting a new access token which is not how it worked previously.  Previously you would get a new refresh token with a new access token.  These changes now check, on an hourly crontab, the expiration date and will refresh the access token and write it to a new encrypted token config file 2 hours before the expiration.

--- python/CheckTokenExpiration.py
+++ python/CheckTokenExpiration.py
@@ -1,59 +1,47 @@
 import configparser
 import os
 import Logger
+import TeslaRefreshToken
 
-from GoogleAPI import getGoogleSheetService
-from SendEmail import sendEmail
-from Crypto import decrypt
+from Crypto import simpleDecrypt
 from datetime import timedelta, datetime
 from io import StringIO
 
 buffer = StringIO(
-  decrypt(
+  simpleDecrypt(
     os.path.join(
       os.path.dirname(os.path.abspath(__file__)),
-      'config.rsa'
+      'token.xor'
     )
   ).decode('utf-8')
 )
 config = configparser.ConfigParser()
 config.sections()
 config.readfp(buffer)
-EV_SPREADSHEET_ID = config['google']['ev_spreadsheet_id']
-EMAIL_1 = config['notification']['email_1']
+EXPIRES_AT = config['tesla']['expires_at']
 buffer.close()
 
 
 ##
-# This checks to see if the access token expires in a week or less and will 
-# send an email reminder to get a new one.
+# This checks to see if the access token is about to expire and will get
+# a new access token from the refresh token.
 #
 # author:  mjhwa@yahoo.com
 ##
 def checkTokenExpiration():
   try:
     # get token expiration date
-    service = getGoogleSheetService()
-    expiration_date = service.spreadsheets().values().get(
-      spreadsheetId=EV_SPREADSHEET_ID,
-      range='Smart Charger!H5'
-    ).execute().get('values', [])[0][0]
-    expiration_date = datetime.strptime(expiration_date, '%m/%d/%Y')
+    expiration_date = datetime.strptime(EXPIRES_AT, '%Y-%m-%d %H:%M:%S')
 
-    # get the date for the reminder (7 days prior)
-    reminder_date = expiration_date - timedelta(7)
-    #print('reminder date: ' + str(reminder_date))
+    # get the refresh date (2 hours prior)
+    refresh_date = expiration_date - timedelta(hours=2)
+    #print('refresh date: ' + str(refresh_date))
     #print('now: ' + str(datetime.today()))
 
-    if (datetime.today() >= reminder_date):
-      message = ('Your Tesla Access Token is expiring on ' 
-                 + str(expiration_date)
-                 + '.  Please generate a new token soon.')
-      sendEmail(EMAIL_1, 'Tesla Access Token Expiring Soon', message, '', '')
+    if (datetime.today() >= refresh_date):
+      TeslaRefreshToken
   except Exception as e:
     Logger.logError('checkTokenExpiration(): ' + str(e))
-  finally:
-    service.close()
 
 
 def main():
--- python/TeslaRefreshToken.py
+++ python/TeslaRefreshToken.py
@@ -4,76 +4,69 @@ import urllib
 import datetime
 import configparser
 import os
-import GoogleAPI
+import tzlocal
 
-from Crypto import decrypt
+from Crypto import simpleDecrypt
+from itertools import cycle, izip
+from datetime import datetime, timedelta
 from io import StringIO
 
 buffer = StringIO(
-  decrypt(
+  simpleDecrypt(
     os.path.join(
       os.path.dirname(os.path.abspath(__file__)),
-      'config.rsa'
+      'token.xor'
     )
   ).decode('utf-8')
 )
 config = configparser.ConfigParser()
 config.sections()
 config.readfp(buffer)
-EV_SPREADSHEET_ID = config['google']['ev_spreadsheet_id']
+REFRESH_TOKEN = config['tesla']['refresh_token']
 buffer.close()
 
-
-refresh_token = raw_input('refresh token: ')
-
 # Exchange bearer token for access token
-url = 'https://owner-api.teslamotors.com/oauth/token'
+url = 'https://auth.tesla.com/oauth2/v3/token'
 payload = {
   'grant_type': 'refresh_token',
-  'refresh_token': refresh_token,
-  'client_id': '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
-  'client_secret': 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3'
+  'client_id': 'ownerapi',
+  'refresh_token': REFRESH_TOKEN,
+  'scope': 'openid email offline_access'
 }
 
-response = requests.post(
+response = json.loads(requests.post(
              url,
              json=payload
-           )
+           ).text)
 
-# print outputs to screen
-print('# access_token=' + json.loads(response.text)['access_token'])
-print(
-  '# expires_at: '
-  + str(
-    datetime.datetime.fromtimestamp(
-      json.loads(response.text)['created_at']
-      + json.loads(response.text)['expires_in']
-    )
-  )
+dt = datetime.now()
+
+# write output to config file
+message =  '[tesla]\n'
+message += 'access_token=' + (response)['access_token'] + '\n'
+message += 'refresh_token=' + (response)['refresh_token'] + '\n'
+message += 'created_at=' + datetime.strftime(dt, '%Y-%m-%d %H:%M:%S') + '\n'
+message += 'expires_at=' + datetime.strftime(tzlocal.get_localzone().localize(dt + timedelta(seconds=(response)['expires_in'])), '%Y-%m-%d %H:%M:%S') + '\n'
+
+# Read key
+key_file = open(
+  os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'token_key'
+  ), 'rb'
 )
-print('# refresh_token: ' + json.loads(response.text)['refresh_token'])
-print(
-  '# created_at: '
-  + str(
-    datetime.datetime.fromtimestamp(
-      json.loads(response.text)['created_at']
-    )
-  )
+key = key_file.read()
+key_file.close()
+
+# Encrypt with key
+encrypted = ''.join(chr(ord(c)^ord(k)) for c,k in izip(message, cycle(key)))
+
+# Write encrypted file
+f = open(
+  os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'token.xor'
+  ), 'wb'
 )
-
-# write timestamp into Google Sheet for crontab expiration checks
-inputs = [{
-  'range': 'Smart Charger!H5',
-  'values': [[datetime.datetime.fromtimestamp(
-    json.loads(response.text)['created_at']
-    + json.loads(response.text)['expires_in']
-  ).strftime('%m/%d/%Y')]]
-}]
-service = GoogleAPI.getGoogleSheetService()
-service.spreadsheets().values().batchUpdate(
-  spreadsheetId=EV_SPREADSHEET_ID,
-  body={'data': inputs, 'valueInputOption': 'USER_ENTERED'}
-).execute()
-service.close()
-
-
+f.write(encrypted)
+f.close()
