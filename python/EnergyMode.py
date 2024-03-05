a4107218dcc0b0914f11f1fcb08fc26a2fd0eb2e
Mike Hwa <mjhwa@yahoo.com>
2024-03-05 13:03:07 -0800

    Fix nesting bug.

--- python/EnergyMode.py
+++ python/EnergyMode.py
@@ -15,8 +15,8 @@ PCT_THRESHOLD = 0.5
 ##
 # Set to run on an early morning cron job (before sunrise) that will 
 # check today and tomorrow's weather and if more than a certain percentage 
-# of rain is forecasted between sunrise and sunset, set the system to 
-# backup mode so it will reserve the battery stored energy and prioritize 
+# of rain is forecasted between sunrise and sunset, set the backup reserve 
+# to 100% so it will reserve the battery stored energy and prioritize 
 # charging the battery in case there is an outage.  There tends not to be 
 # enough solar generation during rainy days for self-powered or time-based
 # control modes while also recharging the battery to 100%.  
@@ -30,64 +30,62 @@ def setEnergyModeBasedOnWeather():
     check_dates = [date.today(), (date.today() + timedelta(1))]
     msg = ''
 
-    for i, value in enumerate(check_dates):
+    for _, val_1 in enumerate(check_dates):
       forecast = ''
       rain = 0
       total = 0
 
       # get sunrise and sunset times 
-      for j, value in enumerate(wdata['daily']):
-        dt = datetime.fromtimestamp(value['dt'])
-        weather = value['weather'][0]['main']
+      for _, val_2 in enumerate(wdata['daily']):
+        dt = datetime.fromtimestamp(val_2['dt'])
 
-        if ((dt.year == check_dates[i].year)
-            and (dt.month == check_dates[i].month)
-            and (dt.day == check_dates[i].day)):
-          sunrise = datetime.fromtimestamp(value['sunrise'])
-          sunset = datetime.fromtimestamp(value['sunset'])
+        if ((dt.year == val_1.year)
+            and (dt.month == val_1.month)
+            and (dt.day == val_1.day)):
+          sunrise = datetime.fromtimestamp(val_2['sunrise'])
+          sunset = datetime.fromtimestamp(val_2['sunset'])
 
-      # loop through the hourly weather matching year, month, day, and 
-      # between the hour values of sunrise and sunset
-      for j, value in enumerate(wdata['hourly']):
-        dt = datetime.fromtimestamp(value['dt'])
+          # loop through the hourly weather matching year, month, day, and 
+          # between the hour values of sunrise and sunset
+          for _, val_3 in enumerate(wdata['hourly']):
+            dt = datetime.fromtimestamp(val_3['dt'])
 
-        if ((dt.year == check_dates[i].year)
-            and (dt.month == check_dates[i].month)
-            and (dt.day == check_dates[i].day)
-            and (dt.hour >= sunrise.hour)
-            and (dt.hour <= sunset.hour)):
+            if ((dt.year == val_1.year)
+                and (dt.month == val_1.month)
+                and (dt.day == val_1.day)
+                and (dt.hour >= sunrise.hour)
+                and (dt.hour <= sunset.hour)):
+              weather = val_3['weather'][0]['main']
+              forecast += str(dt) + ': ' + weather + '\n'
 
-          forecast += str(dt) + ': ' + value['weather'][0]['main'] + '\n'
+              # count how many 'Rain' hours there are
+              if (weather == 'Rain'):
+                rain += 1
 
-          # count how many 'Rain' hours there are
-          if (value['weather'][0]['main'] == 'Rain'):
-            rain += 1
+              # count how many total hours there are between sunrise and sunset
+              total += 1
 
-          # count how many total hours there are between sunrise and sunset
-          total += 1
+				  # if the ratio of rain to non-rain hours is greater than a specified
+				  # percentage, prep content for email
+          if ((float(rain) / float(total)) > PCT_THRESHOLD): 
+            msg += 'Greater than ' + str(int(PCT_THRESHOLD * 100))
+            msg += '% rain forecasted, setting backup reserve to 100%\n'
+            msg += 'Percent rain: ' 
+            msg += str(round(float(rain) / float(total) * 100, 1)) + '%\n'
+            msg += forecast + '\n'
 
-      # if the ratio of rain to non-rain hours is greater than a specified
-      # percentage, set backup reserve to 100%, otherwise set to normal 
-      # backup reserve of 35%
-      if ((float(rain) / float(total)) > PCT_THRESHOLD): 
-        setBackupReserve(100)
-
-        msg += 'Greater than ' + str(int(PCT_THRESHOLD * 100))
-        msg += '% rain forecasted, setting backup reserve to 100%\n'
-        msg += 'Percent rain: ' 
-        msg += str(round(float(rain) / float(total) * 100, 1)) + '%\n'
-        msg += forecast + '\n'
-      else:
-        # if none of the days is above the threshold
-        if (msg == ''):
-          setBackupReserve(35)
-
+	  # if the ratio of rain to non-rain hours for today or tomorrow
+	  # is greater than a specified percentage, set backup reserve to
+	  # 100% and send email, otherwise set to normal backup reserve of 35%
     if (msg != ''):
+      setBackupReserve(100)
       sendEmail(EMAIL_1, 
                 'Energy:  Setting Backup Reserve to 100%', 
                 msg, 
                 '', 
                 '')
+    else:
+      setBackupReserve(35)
   except Exception as e:
     logError('setEnergyModeBasedOnWeather(): ' + str(e))
 
